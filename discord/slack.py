72dbc00ebb996f097547f1e4fbc19c33b2625eaa
RadioLogic <ncottrellweb@gmail.com>
2020-11-30 18:13:21 -0500

    Created slack client class

--- discord/slack.py
+++ discord/slack.py
@@ -1,4 +1,6 @@
 from .http import Route
+from .client import Client
+from .slackhttp import HTTPClient
 
 class SlackRoute(Route):
     """ This is a custom route class to allow connection to slack API rather than discord API
@@ -9,3 +11,7 @@ class SlackRoute(Route):
         # super(self,method, path, **parameters)
         super().__init__(method, path, **parameters)
 
+class Slack(Client):
+    def __init__(self, *, loop=None, **options) -> None:
+        super().__init__(self, loop=None, **options)
+        self.http = HTTPClient(super().connector, proxy=super().proxy, proxy_auth=super().proxy_auth, unsync_clock=super().unsync_clock, loop=self.loop)
