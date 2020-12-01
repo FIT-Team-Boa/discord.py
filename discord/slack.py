70e18ebd539c4900ae08b001eef71388577a719d
RadioLogic <ncottrellweb@gmail.com>
2020-12-01 10:24:09 -0500

    Removed super from httpclient functions

--- discord/slack.py
+++ discord/slack.py
@@ -13,5 +13,9 @@ class SlackRoute(Route):
 
 class Slack(Client):
     def __init__(self, *, loop=None, **options) -> None:
-        super().__init__(self, loop=None, **options)
-        self.http = HTTPClient(super().connector, proxy=super().proxy, proxy_auth=super().proxy_auth, unsync_clock=super().unsync_clock, loop=self.loop)
+        super().__init__(loop=None, **options)
+        connector = options.pop('connector', None)
+        proxy = options.pop('proxy', None)
+        proxy_auth = options.pop('proxy_auth', None)
+        unsync_clock = options.pop('assume_unsync_clock', True)
+        self.http = HTTPClient(connector, proxy=proxy, proxy_auth=proxy_auth, unsync_clock=unsync_clock, loop=self.loop)
